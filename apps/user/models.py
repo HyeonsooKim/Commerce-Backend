bf440872e0b18c47ff6af9e3a771428f7edeba30
HyeonsooKim <gustn9442@naver.com>
2022-09-14 11:56:05 +0900

    Chore: 사용자 데이터 필드 업데이트 - 테이블에 맞게 작성

--- apps/user/models.py
+++ apps/user/models.py
@@ -1,18 +1,19 @@
 from django.db import models
 from django.contrib.auth.models import AbstractBaseUser, BaseUserManager
+from django.core.validators import MinValueValidator
 
 class UserManager(BaseUserManager):
     # 일반 user 생성
-    def create_user(self, email, nickname, name, password=None):
+    def create_user(self, email, username, name, password=None):
         if not email:
             raise ValueError('must have user email')
-        if not nickname:
-            raise ValueError('must have user nickname')
+        if not username:
+            raise ValueError('must have user username')
         if not name:
             raise ValueError('must have user name')
         user = self.model(
             email = self.normalize_email(email),
-            nickname = nickname,
+            username = username,
             name = name
         )
         user.set_password(password)
@@ -20,11 +21,11 @@ class UserManager(BaseUserManager):
         return user
 
     # 관리자 user 생성
-    def create_superuser(self, email, nickname, name, password=None):
+    def create_superuser(self, email, username, name, password=None):
         user = self.create_user(
             email,
             password = password,
-            nickname = nickname,
+            username = username,
             name = name
         )
         user.is_admin = True
@@ -32,11 +33,17 @@ class UserManager(BaseUserManager):
         return user
 
 class User(AbstractBaseUser):
+    GENDER_CHOICES = (('Male', '남성'), ('Female', '여성'))
+
     id = models.AutoField(primary_key=True)
+    username = models.CharField(default='', max_length=100, null=False, blank=False, unique=True)
     email = models.EmailField(default='', max_length=100, null=False, blank=False, unique=True)
-    nickname = models.CharField(default='', max_length=100, null=False, blank=False, unique=True)
     name = models.CharField(default='', max_length=100, null=False, blank=False)
-    
+    city = models.CharField(max_length=100, null=True)
+    age = models.IntegerField(validators=[MinValueValidator(1)], null=True, blank=True)
+    gender = models.CharField(max_length=6, choices=GENDER_CHOICES, null=True, blank=True)
+
+
     # User 모델의 필수 field
     is_active = models.BooleanField(default=True)    
     is_admin = models.BooleanField(default=False)
@@ -44,10 +51,10 @@ class User(AbstractBaseUser):
     # 헬퍼 클래스 사용
     objects = UserManager()
 
-    # 사용자의 username field는 nickname으로 설정
-    USERNAME_FIELD = 'nickname'
+    # 사용자의 username field는 username으로 설정
+    USERNAME_FIELD = 'username'
     # 필수로 작성해야하는 field
     REQUIRED_FIELDS = ['email', 'name']
 
     def __str__(self):
-        return self.nickname
+        return self.username
--- apps/user/serializers.py
+++ apps/user/serializers.py
@@ -2,14 +2,15 @@ from .models import User
 from rest_framework import serializers
 
 class UserSerializer(serializers.ModelSerializer):
+    password = serializers.CharField(write_only=True, max_length=128, style={'input_type': 'password'})
     def create(self, validated_data):
         user = User.objects.create_user(
             email = validated_data['email'],
-            nickname = validated_data['nickname'],
+            username = validated_data['username'],
             name = validated_data['name'],
             password = validated_data['password']
         )
         return user
     class Meta:
         model = User
-        fields = ['nickname', 'email', 'name', 'password']
+        fields = ['username', 'email', 'name', 'password', 'age', 'gender', 'city', 'nationality']
