4471e4c090404bf8bd04eaa8e6d54bc134562144
HyeonsooKim <gustn9442@naver.com>
2022-09-14 15:23:44 +0900

    Chore: url 변경, migrations 초기화, models 변경 - urls 방식 변경(list 조회, detail 페이지 추가) - migrations error로 초기화 후 DB 재연결 - migrations 시도 시 default 값 부재로 인한 에러 발생 -> models에 default값 추가

--- apps/order/urls.py
+++ apps/order/urls.py
@@ -1,9 +1,20 @@
 from django.urls import path, include
 from . import views
-from rest_framework import routers
 
-router = routers.DefaultRouter()
+# order 목록 보여주기
+order_list = views.OrderViewSet.as_view({
+    'get': 'list',
+    'post': 'create'
+})
+
+# order detail 보여주기 + 수정 + 삭제
+order_detail = views.OrderViewSet.as_view({
+    'get': 'retrieve',
+    'put': 'update',
+    'delete': 'destroy'
+})
 
 urlpatterns =[
-    path('', include(router.urls)),
+    path('', order_list),
+    path('<int:pk>', order_detail),
 ]
--- apps/product/urls.py
+++ apps/product/urls.py
@@ -1,10 +1,20 @@
 from django.urls import path, include
 from . import views
-from rest_framework import routers
 
-router = routers.DefaultRouter()
-router.register(r'product', views.ProductViewSet)
+# Product 목록 보여주기
+product_list = views.ProductViewSet.as_view({
+    'get': 'list',
+    'post': 'create'
+})
+
+# Product detail 보여주기 + 수정 + 삭제
+product_detail = views.ProductViewSet.as_view({
+    'get': 'retrieve',
+    'put': 'update',
+    'delete': 'destroy'
+})
 
 urlpatterns =[
-    path('', include(router.urls)),
+    path('', product_list),
+    path('<int:pk>', product_detail),
 ]
