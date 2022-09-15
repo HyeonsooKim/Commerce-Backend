1f2b272d0ee0faaf4b6ca9992fe97147b1d13211
HyeonsooKim <gustn9442@naver.com>
2022-09-15 13:58:46 +0900

    Chore: order app 스키마 변경

--- apps/order/models.py
+++ apps/order/models.py
@@ -3,22 +3,38 @@ from django.db import models
 from django.conf import settings
 
 class Order(models.Model):
+    PAY_STATE = (
+        (0, '결제취소'),
+        (1, '결제완료'),
+        )
+    
+    DELIVERY_STATE = (
+        (0, '배송준비중'),
+        (1, '배송중'),
+        (2, '배송완료'),
+        )
+
     user = models.ForeignKey(
         settings.AUTH_USER_MODEL,
         on_delete=models.CASCADE, )
-    order_date = models.DateTimeField(auto_now_add=True)
-    pay_state = models.CharField(max_length=20, verbose_name='결제상태', default='')
+
+    order_date = models.DateTimeField(auto_now_add=True, verbose_name="날짜")
+    updated_at = models.DateTimeField(verbose_name='수정 날짜', auto_now=True)
+    pay_state = models.PositiveIntegerField(verbose_name='결제 상태', choices=PAY_STATE)
+    delivery_state = models.PositiveIntegerField(verbose_name='배송 상태', choices=DELIVERY_STATE)
     quantity = models.IntegerField(default=1, verbose_name="주문수량")
-    price = models.PositiveIntegerField(default=0, verbose_name='결제금액')
+    price = models.DecimalField(default=0, decimal_places=2, max_digits=10, verbose_name='정가')
+    saled_price = models.DecimalField(default=0,  decimal_places=2, max_digits=10, verbose_name='할인가')
+    delivery_cost = models.DecimalField(verbose_name='배송비', max_digits=10, decimal_places=2, default=1200)
+    payment_amount = models.DecimalField(verbose_name='최종결제액', default=0, max_digits=10, decimal_places=2)
     buyr_city = models.CharField(max_length=100, verbose_name="도시", default='')
     buyr_country = models.CharField(max_length=30, verbose_name='국가코드', default='')
     buyr_zipx = models.CharField(max_length=30, verbose_name='우편번호', default='')
     vccode = models.IntegerField(verbose_name='국가번호', default=0)
-    # products = models.ForeignKey(Product, on_delete=models.CASCADE, related_name='order_product'))
 
     #모델 인스턴스를 주문 날짜 내림차순 정렬
     class Meta:
         ordering = ('-order_date',)
 
-    # def __str__(self):
-    #     return '{} by {}'.format(self.products.name, self.user)
+    def __str__(self):
+        return '{} by {}'.format(self.products.name, self.user.username)
--- apps/order/serializers.py
+++ apps/order/serializers.py
@@ -1,7 +1,45 @@
 from .models import Order
 from rest_framework import serializers
+from rest_framework.serializers import ValidationError
 
 class OrderSerializer(serializers.ModelSerializer):
     class Meta:
         model = Order
         fields = "__all__"
+        read_only_fields = ['id', 'user', 'date', 'quantity',
+                            'price', 'sale_price', 'delivery_cost',
+                            'payment_amount', 'coupon',
+                            'buyr_city', 'buyr_zips', 'vccode']
+
+    def validate(self, data):
+        pay_state = data.get('pay_state', None)
+        delivery_state = data.get('delivery_state', None)
+
+        if pay_state and pay_state not in (0, 1, 2):
+            raise ValidationError("ERROR: 올바르지 않은 결제 상태입니다.")
+        elif delivery_state and delivery_state not in (0, 1, 2, 3):
+            raise ValidationError("ERROR: 올바르지 않은 배송 상태입니다.")
+        return data
+    
+    def update(self, instance, validated_data):
+        """
+        주문 내역 수정 함수
+        pay_state: 0(결제 취소), 1(결제 대기), 2(결제 완료)
+        delivery_state: 0(배송 취소), 1(배송 준비중), 2(배송 중), 3(배송 완료)
+        """
+        pay_state = validated_data.get('pay_state', instance.pay_state)
+        delivery_state = validated_data.get('delivery_state', instance.delivery_state)
+
+        # 결제 취소로 변경하는 경우 쿠폰 사용 초기화
+        # if pay_state == 0 and instance.coupon:
+        #     coupon = Coupon.objects.get(id=instance.coupon)
+        #     coupon.is_used = False
+        #     coupon.date = None
+        #     coupon.sale_amount = None
+        #     coupon.save()
+
+        instance.pay_state = pay_state
+        instance.delivery_state = delivery_state
+        instance.save()
+
+        return instance
